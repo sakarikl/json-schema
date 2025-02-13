2512414c2dd3f8b3e6932a4c9cb509eee2b09aee
Sakari Laine <sakarikl@gmail.com>
2025-02-13 18:33:18 +0200

    string cast

--- src/JsonSchema/Constraints/ObjectConstraint.php
+++ src/JsonSchema/Constraints/ObjectConstraint.php
@@ -66,7 +66,7 @@ class ObjectConstraint extends Constraint
                 continue;
             }
             foreach ($element as $i => $value) {
-                if (preg_match($fullRegex, $i)) {
+                if (preg_match($fullRegex, (string) $i)) {
                     $matches[] = $i;
                     $this->checkUndefined($value, $schema ?: new \stdClass(), $path, $i, in_array($i, $this->appliedDefaults));
                 }
